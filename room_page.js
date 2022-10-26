743cc47a694d8db2e6106a6dd3856607666d6199
MarioAP1 <105328243+MarioAP1@users.noreply.github.com>
2022-10-26 18:36:22 -0400

    Add files via upload

---
+++ room_page.js
@@ -0,0 +1,10 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBpdQUFc1BbQgH-COB5lcxvr71cRaIsEH8",
+    authDomain: "project94-4a840.firebaseapp.com",
+    projectId: "project94-4a840",
+    storageBucket: "project94-4a840.appspot.com",
+    messagingSenderId: "645793005163",
+    appId: "1:645793005163:web:5592cf9e53b499aee6bb2e"
+  };
+  
